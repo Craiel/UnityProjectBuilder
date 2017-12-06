15683c5a9f42200f9d1c1bbb0a86a8676a5ebcc7
Patrick Gustaevel <craieldev@gmail.com>
2017-12-05 19:47:35 -0500

    small change for config

--- Assets/Scripts/SetupCode/EssentialsConfig.cs
+++ Assets/Scripts/SetupCode/EssentialsConfig.cs
@@ -1,8 +1,6 @@
 ﻿namespace Assets.Scripts.SetupCode
 {
-    using Craiel.Essentials;
     using Craiel.Essentials.Contracts;
-    using Craiel.Essentials.Input;
 
     public class EssentialsConfig : IEssentialConfig
     {
@@ -11,15 +9,6 @@
         // -------------------------------------------------------------------
         public void Configure()
         {
-            this.ConfigureInput();
-        }
-
-        // -------------------------------------------------------------------
-        // Private
-        // -------------------------------------------------------------------
-        private void ConfigureInput()
-        {
-            EssentialsCore.DefaultInputState = InputStateDefault.Instance;
         }
     }
 }
